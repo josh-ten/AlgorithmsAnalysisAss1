bbf1347c30bfebfedade7d1360a18723b46cd465
s3657249 <s3657249@student.rmit.edu.au>
2018-03-21 18:10:26 +1100

    AddOwnTestingClass

---
+++ javaSrc/JoshGraphTester.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class JoshGraphTester {
+
+    private static FriendshipGraph<String> graph;
+    
+    public static void main(String args[]) {
+        PrintWriter writer = new PrintWriter(System.out, true);
+        
+        graph = new AdjMatrix<String>();
+        
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addEdge("A", "B");
+        graph.addEdge("C", "A");
+        graph.addEdge("A", "C");
+        graph.addEdge("B", "B");
+        
+        graph.printVertices(writer);
+        graph.printEdges(writer);
+
+        String inputFilename = "";
+        if (inputFilename != "") loadFile(inputFilename);
+    }
+    
+    static void loadFile(String inputFilename) {
+        if (inputFilename != null) {
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(inputFilename));
+                
+                String line;
+                String delimiter = " ";
+                String[] tokens;
+                String srcLabel, tarLabel;
+                
+                while ((line = reader.readLine()) != null) {
+                    tokens = line.split(delimiter);
+                    srcLabel = tokens[0];
+                    tarLabel = tokens[1];
+                    graph.addVertex(srcLabel);
+                    graph.addVertex(tarLabel);
+                    graph.addEdge(srcLabel, tarLabel);
+                }           
+                reader.close();
+            }
+            catch (FileNotFoundException ex) {
+                System.err.println("File " + inputFilename + " not found.");
+            }
+            catch(IOException ex) {
+                System.err.println("Cannot open file " + inputFilename);
+            }
+        }
+    }
+}
